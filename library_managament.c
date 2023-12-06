612635d283e5e78dd259684381338fb2fd78ffc8
fairoozxt <fairoozlubaba.00@gmail.com>
2023-12-06 21:34:59 +0600

    fresh project initiated

---
+++ library_managament.c
@@ -0,0 +1,71 @@
+#include<stdio.h>
+
+void show_welcome();
+
+void show_exit_message();
+
+int authorization_screen();
+
+void show_menu();
+
+void enter_choice();
+
+void add_book();
+
+void show_all_books();
+
+void search_books_by_name();
+
+void search_books_by_author();
+
+int main()
+{
+    printf("hello world");
+    
+    return 0;
+}
+
+void show_welcome()
+{
+
+}
+
+void show_exit_message()
+{
+
+}
+
+int authorization_screen()
+{
+
+}
+ 
+void show_menu()
+{
+
+}
+
+void enter_choice()
+{
+
+}
+
+void add_book()
+{
+
+}
+
+void show_all_books()
+{
+
+}
+
+void search_books_by_name()
+{
+
+}
+
+void search_books_by_author()
+{
+
+}

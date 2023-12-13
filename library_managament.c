2562176d83bfd5c82c1e0010fa2563fce8f40051
fairoozxt <fairoozlubaba.00@gmail.com>
2023-12-13 21:16:47 +0600

    added patterns and search by author function

--- library_managament.c
+++ library_managament.c
@@ -21,7 +21,7 @@ void search_books_by_author();
 int main()
 {
     printf("hello world");
-    
+
     return 0;
 }
 
@@ -39,10 +39,22 @@ int authorization_screen()
 {
 
 }
- 
+
 void show_menu()
 {
+    int choice;
 
+    printf("\n\n1 . Add a Book ");
+    printf("\n2 . Search Book By Name ");
+    printf("\n3 . Search Book By Author ");
+    printf("\n4 . Show All Book Information ");
+    printf("\n5 . Delete a Book ");
+    printf("\n6 . Exit ");
+
+    printf("\n\nPlease Enter Your Choice: ");
+    scanf("%d", &choice);
+
+    return choice;
 }
 
 void enter_choice()
